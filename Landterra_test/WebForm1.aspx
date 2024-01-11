868e32115b6d7d65f72dee9cde3d64eb9fda5cab
Luis <leolivares89@hotmail.com>
2024-01-11 16:47:03 -0600

    Creacion de la capa de vista

---
+++ Landterra_test/WebForm1.aspx
@@ -0,0 +1,121 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="WebForm1.aspx.vb" Inherits="Landterra_test.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+    <style type="text/css">
+        #TextArea1 {
+            width: 241px;
+            height: 78px;
+        }
+    </style>
+</head>
+
+<body>
+    <form id="form1" runat="server">
+        <div style="height: 487px" runat="server" id="Registo">
+            <br />
+            <br />
+            <asp:UpdatePanel ID="UpdatePanel1" runat="server" UpdateMode="Always">
+                <ContentTemplate>
+                    <table style="width: 100%;">
+                        <tr>
+                            <td align="center">
+                                <asp:Label ID="lblTituloFormulario" runat="server" Text="Registro de Emplados."></asp:Label>
+                                <span id="demoCountdown" style="font-weight: bold; color: #59A1E9;">&nbsp;</span>
+                            </td>
+                        </tr>
+                    </table>
+                    <table style="width: 100%;">
+
+
+                        <tr>
+                            <td>Ingrese Nombre Completo del Emplado:</td>
+                            <td>
+                                <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox></td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>Seleccione el tipo de Identificacion:</td>
+                            <td>
+                                <asp:ListBox ID="ListBox1" runat="server" Height="24px" Width="158px">
+                                    <asp:ListItem>Cedula</asp:ListItem>
+                                    <asp:ListItem>Pasaporte</asp:ListItem>
+                                </asp:ListBox></td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>Ingreses numero de Identificacion:</td>
+                            <td>
+                                <asp:TextBox ID="TextBox2" runat="server" Width="170px"></asp:TextBox></td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>Fecha de Ingreso:</td>
+                            <td>
+                                <asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+                            </td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>Direccion:</td>
+                            <td>
+                                <textarea id="TextArea1" cols="20" name="S1" rows="1"></textarea></td>
+                            <td>
+                                <asp:Button ID="Button1" runat="server" Text="Guardar" /></td>
+                        </tr>
+                    </table>
+                </ContentTemplate>
+            </asp:UpdatePanel>
+        </div>
+        <div style="height: 487px" runat="server" id="Vacaciones" visible="false">
+            <br />
+            <br />
+            <asp:UpdatePanel ID="UpdatePanel2" runat="server" UpdateMode="Always">
+                <ContentTemplate>
+                    <table style="width: 100%;">
+                        <tr>
+                            <td align="center">
+                                <asp:Label ID="Label1" runat="server" Text="Registro de Vacaciones"></asp:Label>
+
+                            </td>
+                        </tr>
+                    </table>
+                    <table style="width: 100%;">
+                        <asp:HiddenField ID="hfIDEmpleado" runat="server" />
+                        <tr>
+                            <td>Emplado:</td>
+                            <td>
+                                <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox></td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>Fecha de Inicio:</td>
+                            <td>
+                                <asp:Calendar ID="Calendar2" runat="server"></asp:Calendar>
+                            </td>
+                            <td>Fecha de Fin:</td>
+                            <td>
+                                <asp:Calendar ID="Calendar3" runat="server"></asp:Calendar>
+                            </td>
+
+                        </tr>
+                        <tr>
+                            <td>
+                                <asp:Button ID="Button2" runat="server" Text="Guardar" />
+
+                            </td>
+                        </tr>
+                    </table>
+                </ContentTemplate>
+            </asp:UpdatePanel>
+        </div>
+        <div style="height: 487px">            
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="true"></asp:GridView>
+        </div>
+    </form>
+</bodv>
+</html>
---
+++ Landterra_test/WebForm1.aspx.designer.vb
@@ -0,0 +1,177 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class WebForm1
+
+    '''<summary>
+    '''Control form1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+
+    '''<summary>
+    '''Control Registo.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Registo As Global.System.Web.UI.HtmlControls.HtmlGenericControl
+
+    '''<summary>
+    '''Control UpdatePanel1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents UpdatePanel1 As Global.System.Web.UI.UpdatePanel
+
+    '''<summary>
+    '''Control lblTituloFormulario.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblTituloFormulario As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control TextBox1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TextBox1 As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control ListBox1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents ListBox1 As Global.System.Web.UI.WebControls.ListBox
+
+    '''<summary>
+    '''Control TextBox2.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TextBox2 As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control Calendar1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Calendar1 As Global.System.Web.UI.WebControls.Calendar
+
+    '''<summary>
+    '''Control Button1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Button1 As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Control Vacaciones.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Vacaciones As Global.System.Web.UI.HtmlControls.HtmlGenericControl
+
+    '''<summary>
+    '''Control UpdatePanel2.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents UpdatePanel2 As Global.System.Web.UI.UpdatePanel
+
+    '''<summary>
+    '''Control Label1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Label1 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control hfIDEmpleado.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents hfIDEmpleado As Global.System.Web.UI.WebControls.HiddenField
+
+    '''<summary>
+    '''Control TextBox3.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TextBox3 As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control Calendar2.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Calendar2 As Global.System.Web.UI.WebControls.Calendar
+
+    '''<summary>
+    '''Control Calendar3.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Calendar3 As Global.System.Web.UI.WebControls.Calendar
+
+    '''<summary>
+    '''Control Button2.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Button2 As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Control GridView1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents GridView1 As Global.System.Web.UI.WebControls.GridView
+End Class
